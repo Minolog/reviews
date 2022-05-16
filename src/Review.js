cb89df53d2fa97bdeb979bca471b8e990c50b7f8
Mino <minolog.blog@gmail>
2022-05-16 18:48:07 +0900

    master

---
+++ src/Review.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  const {name,job,image,text} = people[index];
+
+  // 設定されてない番号の時の処理
+  const checkNumber = (number) => {
+    if(number > people.length -1){
+      return 0;
+    }
+    if(number < 0){
+      return people.length -1;
+    }
+    return number;
+  }
+
+  // Nextボタンで次の項目に進む
+  const nextPerson = () => {
+    setIndex((index)=>{
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  // Prevボタンで前の項目に戻る
+  const prevPerson = () => {
+    setIndex((index)=>{
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  // ランダム（もっといい方法ありそう）
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if(randomNumber === index){
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn' onClick={randomPerson}>surprise me</button>
+    </article>
+  );
+};
+
+export default Review;
